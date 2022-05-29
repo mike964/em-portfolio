472068fbb456e044b46315cb120233e3d94f7e57
Amen Kemal <57805638+mike964@users.noreply.github.com>
2022-05-29 15:48:01 +0300

    react slick slider for skills added

--- src/components/sections/SkillsSection.jsx
+++ src/components/sections/SkillsSection.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+// import { Carousel } from '@trendyol-js/react-carousel'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
 
 const SkillsSection = () => {
+	const items = [
+		<SkillItem color='#1af250' title='HTML 5'>
+			<img
+				src='skill-icons/html-5.png'
+				className='mx-auto'
+				alt='html'
+				style={{ height: '120px', width: '120px' }}
+			/>
+		</SkillItem>,
+		<SkillItem color='#1a9cf2' />,
+		<SkillItem color='#f21a1a' />,
+		<SkillItem color='#ffd900' />,
+		<SkillItem color='#1af250' />,
+		<SkillItem color='#1a9cf2' />,
+		<SkillItem color='#f21a1a' />,
+		<SkillItem color='#ffd900' />,
+	]
+
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 4.5,
+		slidesToScroll: 3,
+		nextArrow: <SampleNextArrow />,
+		prevArrow: <SamplePrevArrow />,
+	}
+
 	return (
 		<section id='skills' className='skills'>
 			<div className='container'>
@@ -11,9 +42,47 @@ const SkillsSection = () => {
 						aliquid fuga eum quidem.
 					</p>
 				</div>
-				<div className='row skills-content'></div>
+				<div className='row skills-content'>
+					<div className='col-8 m-auto'>
+						{/* Skills Slider */}
+						<Slider {...settings}>{items}</Slider>
+					</div>
+				</div>
 			</div>
 		</section>
+	)
+}
+
+const SkillItem = ({ children, color, title }) => {
+	return (
+		<div
+			className='text-center'
+			style={{ background: color, height: '150px', width: '150px' }}>
+			<span className='title white'>{title}</span>
+			{children}
+		</div>
+	)
+}
+
+function SampleNextArrow(props) {
+	const { className, style, onClick } = props
+	return (
+		<div
+			className={className}
+			style={{ ...style, display: 'block', background: 'skyblue' }}
+			onClick={onClick}
+		/>
+	)
+}
+
+function SamplePrevArrow(props) {
+	const { className, style, onClick } = props
+	return (
+		<div
+			className={className}
+			style={{ ...style, display: 'block', background: 'skyblue' }}
+			onClick={onClick}
+		/>
 	)
 }
 
